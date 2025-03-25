bf5b801107879d1f9239f01fb5f0dd572774632a
sasha <kireikosasha@gmail.com>
2025-03-25 15:28:02 +0200

    improved cinematic handler

--- src/main/java/kireiko/dev/anticheat/api/player/CinematicComponent.java
+++ src/main/java/kireiko/dev/anticheat/api/player/CinematicComponent.java
@@ -1,59 +1,74 @@
 package kireiko.dev.anticheat.api.player;
 
 import com.google.common.collect.Lists;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 import kireiko.dev.anticheat.api.events.RotationEvent;
 import kireiko.dev.anticheat.utils.GraphUtil;
-
-import java.util.List;
+import kireiko.dev.millennium.math.Statistics;
 
 public final class CinematicComponent {
-
-    private final List<Double> yawSamples = Lists.newArrayList();
-    private final List<Double> pitchSamples = Lists.newArrayList();
-    private final PlayerProfile profile;
     private long lastSmooth = 0L, lastHighRate = 0L;
-    private double lastDeltaYaw = 0.0d, lastDeltaPitch = 0.0d;
+    private double lastDeltaYaw = 0.0D, lastDeltaPitch = 0.0D;
+    private final List<Double
+                    > yawSamples = Lists.newArrayList(),
+                    pitchSamples = Lists.newArrayList();
+    private final PlayerProfile profile;
+    private int isTotallyNotCinematic = 0;
 
-    public CinematicComponent(final PlayerProfile profile) {
+    public CinematicComponent(PlayerProfile profile) {
         this.profile = profile;
     }
 
-    public void process(final RotationEvent rotationUpdate) {
-        final long now = System.currentTimeMillis();
-
-        final double deltaYaw = rotationUpdate.getDelta().getX();
-        final double deltaPitch = rotationUpdate.getDelta().getY();
-
-        final double differenceYaw = Math.abs(deltaYaw - lastDeltaYaw);
-        final double differencePitch = Math.abs(deltaPitch - lastDeltaPitch);
-
-        final double joltYaw = Math.abs(differenceYaw - deltaYaw);
-        final double joltPitch = Math.abs(differencePitch - deltaPitch);
-
-        final boolean cinematic = (now - lastHighRate > 250L) || now - lastSmooth < 9000L;
-
-        if (joltYaw > 1.0 && joltPitch > 1.0) {
+    public void process(RotationEvent rotationUpdate) {
+        long now = System.currentTimeMillis();
+        double deltaYaw = rotationUpdate.getDelta().getX();
+        double deltaPitch = rotationUpdate.getDelta().getY();
+        double differenceYaw = Math.abs(deltaYaw - this.lastDeltaYaw);
+        double differencePitch = Math.abs(deltaPitch - this.lastDeltaPitch);
+        double joltYaw = Math.abs(differenceYaw - deltaYaw);
+        double joltPitch = Math.abs(differencePitch - deltaPitch);
+        boolean cinematic = (now - this.lastHighRate > 250L || now - this.lastSmooth < 9000L);
+        if (joltYaw > 1.0D && joltPitch > 1.0D)
             this.lastHighRate = now;
-        }
-        yawSamples.add(deltaYaw);
-        pitchSamples.add(deltaPitch);
-        if (yawSamples.size() == 20 && pitchSamples.size() == 20) {
-            final GraphUtil.GraphResult resultsYaw = GraphUtil.getGraph(yawSamples);
-            final GraphUtil.GraphResult resultsPitch = GraphUtil.getGraph(pitchSamples);
-            final int negativesYaw = resultsYaw.getNegatives();
-            final int negativesPitch = resultsPitch.getNegatives();
-            final int positivesYaw = resultsYaw.getPositives();
-            final int positivesPitch = resultsPitch.getPositives();
-            if (positivesYaw > negativesYaw || positivesPitch > negativesPitch) {
-                this.lastSmooth = now;
+        this.yawSamples.add(deltaYaw);
+        this.pitchSamples.add(deltaPitch);
+        if (this.yawSamples.size() >= 20 && this.pitchSamples.size() >= 20) {
+            Set<Double> shannonYaw = new HashSet<>(), shannonPitch = new HashSet<>();
+            List<Double> stackYaw = new ArrayList<>(), stackPitch = new ArrayList<>();
+            for (Double yawSample : this.yawSamples) {
+                stackYaw.add(yawSample);
+                stackPitch.add(yawSample);
+                if (stackYaw.size() >= 10 && stackPitch.size() >= 10) {
+                    shannonYaw.add(Statistics.getShannonEntropy(stackYaw));
+                    shannonPitch.add(Statistics.getShannonEntropy(stackPitch));
+                    stackYaw.clear();
+                    stackPitch.clear();
+                }
             }
-
-            yawSamples.clear();
-            pitchSamples.clear();
+            if (shannonYaw.size() != 1 || shannonPitch.size() != 1 || (
+                            (Double) shannonYaw.toArray()[0]).doubleValue() != ((Double) shannonPitch.toArray()[0]).doubleValue()) {
+                this.isTotallyNotCinematic = 20;
+            }
+            GraphUtil.GraphResult resultsYaw = GraphUtil.getGraph(this.yawSamples);
+            GraphUtil.GraphResult resultsPitch = GraphUtil.getGraph(this.pitchSamples);
+            int negativesYaw = resultsYaw.getNegatives();
+            int negativesPitch = resultsPitch.getNegatives();
+            int positivesYaw = resultsYaw.getPositives();
+            int positivesPitch = resultsPitch.getPositives();
+            if (positivesYaw > negativesYaw || positivesPitch > negativesPitch)
+                this.lastSmooth = now;
+            this.yawSamples.clear();
+            this.pitchSamples.clear();
         }
-
-        profile.setCinematic(cinematic);
-
+        if (this.isTotallyNotCinematic > 0) {
+            this.isTotallyNotCinematic--;
+            this.profile.setCinematic(false);
+        } else {
+            this.profile.setCinematic(cinematic);
+        }
         this.lastDeltaYaw = deltaYaw;
         this.lastDeltaPitch = deltaPitch;
     }
